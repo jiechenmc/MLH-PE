70b682efcf243a5fd25288f48b01bc967ca51500
Jie Chen <jiechenmc@gmail.com>
2022-06-17 05:28:34 +0000

    added redeploy script

---
+++ redeploy-site.sh
@@ -0,0 +1,10 @@
+# Updates
+tmux kill-server
+cd project-best-project
+git fetch && git reset origin/main --hard
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+
+# Make new tmux session
+tmux new-session -d 'export FLASK_ENV=development && flask run --host=0.0.0.0'
+tmux split-window -h 'cd client && npm run dev -- --host'
