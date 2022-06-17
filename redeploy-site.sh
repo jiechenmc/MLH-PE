2500efb12f282b206c0aced19a9f8fb22e2a3a15
Jie Chen <jiechenmc@gmail.com>
2022-06-17 19:12:59 +0000

    added pip upgrade to redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,6 +3,7 @@ tmux kill-server
 cd project-best-project
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Make new tmux session
