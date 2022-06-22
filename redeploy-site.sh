cd7f51dc408ddadebe540ea9b077bd7321a02ed5
Jie Chen <jiechenmc@gmail.com>
2022-06-21 22:24:47 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,6 +6,5 @@ source python3-virtualenv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Make new tmux session
 systemctl restart myportfolio
 tmux new -d 'cd client && npm run dev -- --host'
