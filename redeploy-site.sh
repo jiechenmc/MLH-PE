1e84a426795fe7fe6fe55df9483001f4fe10dc3d
Jie Chen <jiechenmc@gmail.com>
2022-06-22 02:23:33 +0000

    new script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,11 +2,10 @@
 tmux kill-server
 cd project-best-project
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
 # Make new tmux session
-tmux new-session -d 'flask run --host=0.0.0.0'
-tmux split-window -h 'cd client && npm run dev -- --host'
+systemctl restart myportfolio
+tmux new -d 'cd client && npm run dev -- --host'
