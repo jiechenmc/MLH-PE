b67a070ea3c42bfffa545ae6afdadc01fc539fdf
Jie Chen <jiechenmc@gmail.com>
2022-07-04 22:29:02 -0500

    react container

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,9 +2,6 @@
 tmux kill-server
 cd project-best-project
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
 
-systemctl restart myportfolio
-tmux new -d 'cd client && npm run dev -- --host'
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
