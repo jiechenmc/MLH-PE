efb8a2d0f4598e17be61f2d94f13829296fc1558
Jie Chen <jiechenmc@gmail.com>
2022-07-11 16:47:44 -0500

    fixed tests

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,7 @@ class TimelinePost(Model):
 db.create_tables([TimelinePost], safe=True)
 
 
-@app.route("/timeline_post", methods=["GET", "POST", "DELETE"])
+@app.route("/api/timeline_post", methods=["GET", "POST", "DELETE"])
 def timeline_post():
     if request.method == "POST":
         if 'date' not in request.form:
@@ -68,67 +68,3 @@ def index():
                                         'http://localhost:5000').replace(
                                             ":5000", ":3000"),
                     code=301)
-
-
-@app.route("/api/linkedin", methods=["GET"])
-def linkedin():
-    return jsonify({
-        "username": "jiechen-sbu",
-        "url": "https://www.linkedin.com/in/jiechen-sbu/"
-    })
-
-
-@app.route("/api/github", methods=["GET"])
-def github():
-    return jsonify({
-        "username": "jiechenmc",
-        "url": "https://github.com/jiechenmc"
-    })
-
-
-@app.route("/api/resume", methods=["GET"])
-def resume():
-    return jsonify({
-        "url":
-        "https://drive.google.com/file/d/1YYvgQczc_2N1tn6oSFrAqZYatgVpykkL/view"
-    })
-
-
-@app.route("/api/journey", methods=["GET"])
-def journey():
-    return jsonify([{
-        "date": "August 2021",
-        "title": "Fall of Freshman Year",
-        "events": ["Introduction to Object-Oriented Programming"]
-    }, {
-        "date":
-        "January 2022",
-        "title":
-        "Spring of Freshman Year",
-        "events": ["Data Structures\n", "Foundations of Computer Science"]
-    }, {
-        "date": "May 2022",
-        "title": "Summer of Freshman Year",
-        "events": ["MLH Production Engineering Fellowship!"]
-    }])
-
-
-@app.route("/api/projects", methods=["GET"])
-def projects():
-    return jsonify([
-        {
-            "title": "Pokemon Data Explorer",
-            "date": "May 2022",
-            "description": "Pokedex clone?",
-            "URL": "https://github.com/jiechenmc/poke_data_explorer",
-            "status": "incomplete"
-        },
-    ])
-
-
-# Wrapping these commands so tests/test_db.py can import TimelinePost without
-# the db and app trying to connect and run.
-if __name__ == '__main__':
-    db.connect()
-    db.create_tables([TimelinePost])
-    app.run()
