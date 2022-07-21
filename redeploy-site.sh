9f9cc02fb4a496e47e0d60d2ea2024b4fa4eb5ed
Jie Chen <jiechenmc@gmail.com>
2022-07-20 22:39:48 -0500

    added rate limit

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,5 @@
 cd /root/project-best-project
 git fetch && git reset origin/main --hard
 
-docker system prune -a -f
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
